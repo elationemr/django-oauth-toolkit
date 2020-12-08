06736d6fd09213f99a6e7fe06e3e0f68702ce055
Jon Tan <jon08192@gmail.com>
2020-12-07 21:46:12 -0600

    Backport 1.3+ to Django 1.11

--- oauth2_provider/urls.py
+++ oauth2_provider/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.conf.urls import url
 
 from . import views
 
@@ -7,23 +7,23 @@ app_name = "oauth2_provider"
 
 
 base_urlpatterns = [
-    re_path(r"^authorize/$", views.AuthorizationView.as_view(), name="authorize"),
-    re_path(r"^token/$", views.TokenView.as_view(), name="token"),
-    re_path(r"^revoke_token/$", views.RevokeTokenView.as_view(), name="revoke-token"),
-    re_path(r"^introspect/$", views.IntrospectTokenView.as_view(), name="introspect"),
+    url(r"^authorize/$", views.AuthorizationView.as_view(), name="authorize"),
+    url(r"^token/$", views.TokenView.as_view(), name="token"),
+    url(r"^revoke_token/$", views.RevokeTokenView.as_view(), name="revoke-token"),
+    url(r"^introspect/$", views.IntrospectTokenView.as_view(), name="introspect"),
 ]
 
 
 management_urlpatterns = [
     # Application management views
-    re_path(r"^applications/$", views.ApplicationList.as_view(), name="list"),
-    re_path(r"^applications/register/$", views.ApplicationRegistration.as_view(), name="register"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/$", views.ApplicationDetail.as_view(), name="detail"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/delete/$", views.ApplicationDelete.as_view(), name="delete"),
-    re_path(r"^applications/(?P<pk>[\w-]+)/update/$", views.ApplicationUpdate.as_view(), name="update"),
+    url(r"^applications/$", views.ApplicationList.as_view(), name="list"),
+    url(r"^applications/register/$", views.ApplicationRegistration.as_view(), name="register"),
+    url(r"^applications/(?P<pk>[\w-]+)/$", views.ApplicationDetail.as_view(), name="detail"),
+    url(r"^applications/(?P<pk>[\w-]+)/delete/$", views.ApplicationDelete.as_view(), name="delete"),
+    url(r"^applications/(?P<pk>[\w-]+)/update/$", views.ApplicationUpdate.as_view(), name="update"),
     # Token management views
-    re_path(r"^authorized_tokens/$", views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
-    re_path(
+    url(r"^authorized_tokens/$", views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+    url(
         r"^authorized_tokens/(?P<pk>[\w-]+)/delete/$",
         views.AuthorizedTokenDeleteView.as_view(),
         name="authorized-token-delete",
